7c3b07a471bda74fde1abdb6aaebdd136499ee32
Camsaka <47027569+Camsaka@users.noreply.github.com>
2026-03-14 00:05:18 +0100

    Update headers next.config.js
    Changement du header pour autoriser la fonction eval()

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "script-src 'self' 'unsafe-eval';",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
